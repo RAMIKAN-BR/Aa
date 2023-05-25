7c52c7fcd515980b38dd753e4ca6aaad659581e5
Júlio César <132024683+RAMIKAN-BR@users.noreply.github.com>
2023-05-25 11:04:58 -0300

    AaBaseVisitor.java
    Criado automaticamente pelo Antlr4

---
+++ AaBaseVisitor.java
@@ -0,0 +1,24 @@
+// Generated from Aa.g4 by ANTLR 4.12.0
+
+import java.util.*;
+
+import org.antlr.v4.runtime.tree.AbstractParseTreeVisitor;
+
+/**
+ * This class provides an empty implementation of {@link AaVisitor},
+ * which can be extended to create a visitor which only needs to handle a subset
+ * of the available methods.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+@SuppressWarnings("CheckReturnValue")
+public class AaBaseVisitor<T> extends AbstractParseTreeVisitor<T> implements AaVisitor<T> {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitStart(AaParser.StartContext ctx) { return visitChildren(ctx); }
+}
