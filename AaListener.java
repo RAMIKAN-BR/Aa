324488ea1259b677295c53a964a975e3baa61f8a
Júlio César <132024683+RAMIKAN-BR@users.noreply.github.com>
2023-05-25 11:14:23 -0300

    AaListener.java
    Criado automaticamente pelo Antlr4

---
+++ AaListener.java
@@ -0,0 +1,22 @@
+// Generated from Aa.g4 by ANTLR 4.12.0
+
+import java.util.*;
+
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+
+/**
+ * This interface defines a complete listener for a parse tree produced by
+ * {@link AaParser}.
+ */
+public interface AaListener extends ParseTreeListener {
+	/**
+	 * Enter a parse tree produced by {@link AaParser#start}.
+	 * @param ctx the parse tree
+	 */
+	void enterStart(AaParser.StartContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link AaParser#start}.
+	 * @param ctx the parse tree
+	 */
+	void exitStart(AaParser.StartContext ctx);
+}
